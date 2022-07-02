4d705e36a267024e672aeb6f16e7cbda5f140fc2
Luke Turner <github@luketurner.org>
2022-07-01 17:22:24 -0700

    try using a store

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,85 @@
-import { Component, createSignal } from 'solid-js';
+import { Accessor, Component, For } from 'solid-js';
+import { createStore, produce } from 'solid-js/store';
 
 import styles from './App.module.css';
 
+interface ContainerCartItem {
+  type: 'container'
+  cpu: number
+  memory: number
+  quantity: number
+}
+
+interface NetworkCartItem {
+  type: 'network'
+  bytesIn?: number
+  bytesOut?: number
+}
+
+type CartItem = ContainerCartItem | NetworkCartItem
+
+interface Cart {
+  items: CartItem[]
+}
+
+const [cart, setCart] = createStore<Cart>({
+  items: []
+})
+
+const addCartItem = () => {
+  setCart(
+    produce((cart) => {
+      cart.items.push({
+        type: 'container',
+        cpu: 1,
+        memory: 256,
+        quantity: 1
+      })
+    })
+  )
+}
+
+const deleteCartItem = (index: number) => {
+  setCart(
+    produce((cart) => {
+      cart.items.splice(index, 1)
+    })
+  )
+}
+
+interface CartItemProps {
+  item: CartItem
+  index: Accessor<number>
+}
+
+const CartItem: Component<CartItemProps> = ({ item, index }) => {
+
+  return <div>
+    {index() + 1}. {JSON.stringify(item)}
+    <button onClick={[deleteCartItem, index()]}>(X)</button>
+  </div>
+}
+
+const AddItemButton = () => {
+  return <button onClick={addCartItem}>Add item</button>;
+}
+
+const CartList = () => {
+  return <For each={cart.items} fallback={'Empty cart!'}>
+    {(item, index) => (
+      <CartItem item={item} index={index}></CartItem>
+    )}
+  </For>
+}
 
 const App: Component = () => {
 
-  const [cores, setCores] = createSignal(1);
-  const [memory, setMemory] = createSignal(256);
-
   return (
     <div>
-      <label for="cores">Number of Cores: {cores()}</label>
-      <input type="number" value={cores()} name="cores" onChange={(e) => setCores(parseInt(e.target.value, 10))} />
-      <label for="memory">Memory: {memory()}</label>
-      <input type="number" value={memory()} name="memory" onChange={(e) => setMemory(parseInt(e.target.value || "", 10))} />
-
+      <CartList />
+      <div>
+        <AddItemButton />
+      </div>
     </div>
   );
 };
