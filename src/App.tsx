536f7cc0fb0999e5befceb9af9b1c7c0e6ea4880
Luke Turner <github@luketurner.org>
2022-09-29 20:51:07 -0700

    add about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Component, Show } from 'solid-js';
+import { Component, createSignal, Match, Show, Switch } from 'solid-js';
+import { AboutPage } from './about';
 import { AppDBProvider, useDb } from './db';
 import { ProviderCostBreakdowns, ProviderCostSummaries } from './providers';
 import { ServiceRequestForm } from './ServiceRequestForm';
@@ -17,16 +18,38 @@ const CostInformation = () => {
   );
 }
 
+const MainPage: Component = () => {
+  return (
+    <>
+      <ServiceRequestForm />
+      <CostInformation />
+      <div class="text-center text-slate-600">
+        Prices are hardcoded and may become outdated. (Last update: 2022-09-26)
+      </div>
+    </>
+  )
+}
+
 const App: Component = () => {
+
+  const [page, setPage] = createSignal<'main' | 'about'>('main');
+
   return (
     <AppDBProvider>
       <div class="container mx-auto px-4 max-w-xl border-t-8 border-t-slate-400">
         <h1 class="text-2xl m-2 text-center">PaaS Price Estimator</h1>
-        <ServiceRequestForm />
-        <CostInformation />
-        <div class="text-center text-slate-600">
-          Prices are hardcoded and may become outdated. (Last update: 2022-09-26)
+        <div class="text-center">
+          <button disabled={page() === 'main'} classList={{'text-slate-500': page() === 'main'}} onClick={() => setPage('main')}>main</button> / 
+          <button disabled={page() === 'about'} classList={{'text-slate-500': page() === 'about'}} onClick={() => setPage('about')}>about</button>
         </div>
+        <Switch>
+          <Match when={page() === 'about'}>
+            <AboutPage />
+          </Match>
+          <Match when={page() === 'main'}>
+            <MainPage />
+          </Match>
+        </Switch>
         <div class="text-center text-slate-600">
           Copyright 2022 Luke Turner -- MIT Licensed
         </div>
---
+++ src/about.tsx
@@ -0,0 +1,31 @@
+export const AboutPage = () => {
+  return (
+    <div class="my-2 text-slate-700">
+      <p class="my-2">
+        PaaS Price Estimator is a tool for doing approximate, "back of the napkin" style estimation and comparison of prices across a variety of PaaS (Platform as a Service) vendors. 
+        For example, ever wondered whether an application would be cheaper on Render versus Fly.io? Or which vendors have the cheapest outbound network traffic? 
+        This tool is designed to answer those types of questions.
+      </p>
+      <p class="my-2">
+        How it works: Add one or more containers to your "wishlist." Configure the CPU Type (shared vs. dedicated), number of CPUs, and amount of memory that you want. 
+        For each provider, the app will show the price you'd have to pay to satisfy those resource requirements.
+      </p>
+      <p class="my-2">
+        If the provider doesn't support containers with exactly the specified CPU/memory, the next-highest scale container will be chosen
+        (e.g. if you request 3.5 GB of memory, you will see prices for 4 GB containers for some providers.)
+      </p>
+      <p class="my-2">
+        You can also attach static IPs, network bandwidth, and SSD block storage to your containers, which will be factored into the prices shown in the app.
+        The Cost Breakdown section will show an itemized breakdown of where the total price comes from for each provider.
+      </p>
+      <p class="my-2">
+        Be aware, this is a <strong>work in progress</strong>. Notable gaps:
+      </p>
+      <ul class="list-disc ml-8">
+        <li>Free tiers are currently not included in estimates.</li>
+        <li>Only subset of the desired providers are supported.</li>
+        <li>Only containers are supported. (Want to add managed databases as well.)</li>
+      </ul>
+    </div>
+  )
+};
