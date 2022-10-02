7150aa53d74623deeac9f945697fd3c97946786a
Luke Turner <github@luketurner.org>
2022-10-01 18:26:19 -0700

    improve pricing for fargate fractional vCPUs

--- src/providers/fargate.ts
+++ src/providers/fargate.ts
@@ -7,16 +7,19 @@ export const prices: PricingTable = {
   lastUpdated: '2022-09-30',
   tiers: [
     // TODO -- factional CPUs should be countable as "shared"?
-    costed({ name: '0.25-cpu 512mb', cpu: 0.25, ct: 'de', mem: 512,  costPerSecond: 0 }),
-    costed({ name: '0.25-cpu 1gb',   cpu: 0.25, ct: 'de', mem: 1024, costPerSecond: 0 }),
-    costed({ name: '0.25-cpu 2gb',   cpu: 0.25, ct: 'de', mem: 2048, costPerSecond: 0 }),
+    // Currently, set "shared" = true and "cpu" = 1, even for fractional vCPUs,
+    // because intuitively that's what a user is asking for when they select "1 shared CPU"
+    // Surely a better way to handle this though.
+    costed({ name: '0.25-cpu 512mb', cpu: 1, ct: 'sh', mem: 512  }, 0.25),
+    costed({ name: '0.25-cpu 1gb',   cpu: 1, ct: 'sh', mem: 1024 }, 0.25),
+    costed({ name: '0.25-cpu 2gb',   cpu: 1, ct: 'sh', mem: 2048 }, 0.25),
 
-    ...tiers({ name: '', cpu: 0.5, ct: 'de', mem: [     1024,     1024,   4 * 1024], costPerSecond: 0 }),
-    ...tiers({ name: '', cpu: 1,   ct: 'de', mem: [ 2 * 1024,     1024,   8 * 1024], costPerSecond: 0 }),
-    ...tiers({ name: '', cpu: 2,   ct: 'de', mem: [ 4 * 1024,     1024,  16 * 1024], costPerSecond: 0 }),
-    ...tiers({ name: '', cpu: 4,   ct: 'de', mem: [ 8 * 1024,     1024,  30 * 1024], costPerSecond: 0 }),
-    ...tiers({ name: '', cpu: 8,   ct: 'de', mem: [16 * 1024, 4 * 1024,  60 * 1024], costPerSecond: 0 }),
-    ...tiers({ name: '', cpu: 16,  ct: 'de', mem: [32 * 1024, 8 * 1024, 120 * 1024], costPerSecond: 0 }),
+    ...tiers({ cpu: 1,  ct: 'sh', mem: [     1024,     1024,   4 * 1024], tmpCpuFractional: 0.5 }),
+    ...tiers({ cpu: 1,  ct: 'de', mem: [ 2 * 1024,     1024,   8 * 1024] }),
+    ...tiers({ cpu: 2,  ct: 'de', mem: [ 4 * 1024,     1024,  16 * 1024] }),
+    ...tiers({ cpu: 4,  ct: 'de', mem: [ 8 * 1024,     1024,  30 * 1024] }),
+    ...tiers({ cpu: 8,  ct: 'de', mem: [16 * 1024, 4 * 1024,  60 * 1024] }),
+    ...tiers({ cpu: 16, ct: 'de', mem: [32 * 1024, 8 * 1024, 120 * 1024] }),
 
   ],
   // NOTE -- net is actually tiered.
@@ -39,17 +42,17 @@ export const prices: PricingTable = {
   staticIpPerHour: 0.005,
 }
 
-function costed(tier: PricingTier): PricingTier {
+function costed(tier: PricingTier, tmpCpuFractional?: number): PricingTier {
   // 0.04048 per vCPU-hour, 0.004445 per GB-hour
   // based on us-west-1 region
-  return { ...tier, costPerHour: 0.04048 * tier.cpu + 0.004445 * (tier.mem / 1024) };
+  return { ...tier, costPerHour: 0.04048 * (tmpCpuFractional ?? tier.cpu) + 0.004445 * (tier.mem / 1024) };
 }
 
 function tiers(x): PricingTier[] {
   const tiers = [];
 
   for (let i = x.mem[0]; i <= x.mem[2]; i += x.mem[1]) {
-    tiers.push(costed({ ...x, mem: i, name: `${x.cpu}-cpu ${i / 1024}gb` }))
+    tiers.push(costed({ ...x, mem: i, name: `${x.tmpCpuFractional ?? x.cpu}-cpu ${i / 1024}gb` }))
   }
 
   return tiers;
