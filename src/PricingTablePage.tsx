e048be49be105ab1d75d3984c1d37dbf6cd6cb8e
Luke Turner <github@luketurner.org>
2022-10-13 19:26:53 -0700

    tables page: improve tier table readability (hopefully)

--- src/PricingTablePage.tsx
+++ src/PricingTablePage.tsx
@@ -5,18 +5,32 @@ import { Cost, CostRate, isCostRate, TieredCost } from "./util";
 export interface PricingTableProps extends Provider {
 }
 
+export const TieredCostTable: Component<{
+  value: TieredCost;
+  unit?: string;
+}> = (props) => {
+  if (props.value.length === 0) return undefined; 
+  const firstTier = props.value[0];
+  return (
+    <>
+      <p class="ml-6"><Cost value={firstTier.cost} unit={props.unit}/> :: First {firstTier.size}{props.unit && ` ${props.unit}`}</p>
+      <For each={props.value.slice(1) as TieredCost}>
+        {(t => (
+          <p class="ml-6"><Cost value={t.cost} unit={props.unit}/> :: {t.size === Infinity ? <>Further {props.unit ?? 'GiB'}s</> : <>Next {t.size} {props.unit ?? 'GiB'}</>}</p>
+        ))}
+      </For>
+    </>
+  )
+}
+
 export const CostOrTiered: Component<{
   value: CostRate | TieredCost;
   unit?: string;
 }> = (props) => {
   return (
     <>
-      {isCostRate(props.value) ? <Cost value={props.value} unit={props.unit} /> :
-        <For each={props.value as TieredCost}>
-          {(t => (
-            <p class="ml-6"><Cost value={t.cost} unit={props.unit}/> :: {t.size}</p>
-          ))}
-        </For>
+      {isCostRate(props.value) ? <><Cost value={props.value} unit={props.unit} /></> :
+        <TieredCostTable value={props.value} unit={props.unit} />
       }
     </>
   );
@@ -32,7 +46,7 @@ export const PricingTableView: Component<PricingTableProps> = (props) => {
       <p class="mb-2"></p>
       <p><span class="w-28 inline-block">Persistent SSD</span><CostOrTiered value={props.prices.storage.persistentSsd} unit="GiB-mo"/></p>
       <p><span class="w-28 inline-block">Network out</span><CostOrTiered value={props.prices.net.gbOut} unit="GiB"/></p>
-      <p><span class="w-28 inline-block">Static IP</span><CostOrTiered value={props.prices.staticIp} /></p>
+      <p><span class="w-28 inline-block">Static IP</span><CostOrTiered value={props.prices.staticIp} unit="IP" /></p>
       <p><span class="w-28 inline-block">Container tiers</span></p>
       <For each={props.prices.container}>
         {(tier) => (
