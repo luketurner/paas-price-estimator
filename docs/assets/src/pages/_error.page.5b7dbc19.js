91497d7faf785631e2492e511d41fad75749adf7
Luke Turner <github@luketurner.org>
2022-10-24 13:22:46 -0700

    rebuild

--- docs/assets/src/pages/_error.page.5b7dbc19.js
+++
@@ -1,1 +0,0 @@
-import{c as t,g as e,t as o}from"../../chunk-177323d6.js";import{L as r}from"../../chunk-1face38c.js";const a=o("<p>Not Found!</p>"),p=()=>t(r,{get children(){return e(a)}});export{p as Page};
